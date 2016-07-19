c735c533c27b30715e54d253b5f10616633dfe6a
Sagar Bhandari <webgig@Sagars-iMac.local>
2016-07-19 10:05:06 +1000

    Removes config.php

--- tests/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array("merchant_id" => 4, 
-             "merchant_key" => "4mod1Yim1GEv+D5YOCfSDT4aBEUZErQYMJ3EtdOGhQY=",
-             "environment" => "sandbox",
-             "webhook_endpoint" => "http://localhost:8000/tests/webhook.server.php",
-             "express_endpoint" => "http://localhost:8000/tests/express.server.php" 
-             );
-
